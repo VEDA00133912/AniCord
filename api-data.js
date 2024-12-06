f2dec6b51336643c215efc116ac016eaf2c04744
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-12-06 21:24:44 +0900

    Update api-data.js

--- api-data.js
+++ api-data.js
@@ -1,31 +1,36 @@
-const axios = require("axios");
-const xml2js = require("xml2js");
+const axios = require('axios');
+const xml2js = require('xml2js');
 
-async function getData(id) {
+async function getRandomAnime() {
   try {
     let data = await axios.get(
-      "https://cal.syoboi.jp/db.php?Command=TitleLookup&Fields=Title&TID=" + id
+      'https://cal.syoboi.jp/db.php?Command=TitleLookup&Fields=TID,Title,Cat'
     );
+
     return new Promise((resolve, reject) => {
       xml2js.parseString(data.data, (err, result) => {
         if (err) {
-          return reject("Error parsing XML: " + err.message);
+          return reject('Error parsing XML: ' + err.message);
         }
+
         const titleItems =
           result.TitleLookupResponse.TitleItems?.[0]?.TitleItem;
         if (titleItems && titleItems.length > 0) {
-          const item = titleItems[0];
-          const title = item.Title[0];
-          const itemId = item.$.id;
-          resolve({ itemId, title });
+          const randomIndex = Math.floor(Math.random() * titleItems.length);
+          const item = titleItems[randomIndex];
+          const tid = item.TID?.[0];
+          const title = item.Title?.[0];
+
+          resolve({ tid, title });
         } else {
           resolve(null);
         }
       });
     });
   } catch (error) {
-    console.error("Error fetching data:", error.message);
+    console.error('Error fetching data:', error.message);
     throw error;
   }
 }
-module.exports = getData;
+
+module.exports = getRandomAnime;
